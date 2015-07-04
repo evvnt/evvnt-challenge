5b797cb827f9ebf36d56f300da6a298c46158024
Jamie Barton <jamie@codethisway.com>
2015-07-04 12:06:16 +0100

    Added basic keywords search
    I’d probably use Elasticsearch for a real life application due to the
    amount of data that doesn’t change.

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,6 +1,6 @@
 class API::EventsController < ApplicationController
   def index
-    @event_search_service = EventSearchService.new(params[:venue_ids], params[:start_time], params[:end_time], params[:page])
+    @event_search_service = EventSearchService.new(params[:keywords], params[:venue_ids], params[:start_time], params[:end_time], params[:page])
     render json: @event_search_service.json_results
   end
 end
--- app/services/event_search_service.rb
+++ app/services/event_search_service.rb
@@ -1,7 +1,8 @@
 class EventSearchService
-  attr_accessor :venue_ids, :start_time, :end_time, :page, :results, :total_entries
+  attr_accessor :keywords, :venue_ids, :start_time, :end_time, :page, :results, :total_entries
 
-  def initialize(venues, start_time, end_time, page)
+  def initialize(keywords, venues, start_time, end_time, page)
+    @keywords = keywords
     @venue_ids = venues.nil? ? [] : venues
     @start_time = start_time
     @end_time = end_time
@@ -13,9 +14,11 @@ class EventSearchService
 
   def get_events
     query = Event.scoped
+    query = query.joins(:venue)
+    query = query.where('events.keywords like ? or events.title like ?', "%#{@keywords}%", "%#{@keywords}%") if !@keywords.blank?
     query = query.where(venue_id: @venue_ids) if @venue_ids && @venue_ids.length > 0
-    query = query.where('start_time >= ?', (DateTime.parse(@start_time)).iso8601) if !@start_time.blank?
-    query = query.where('end_time <= ?', (DateTime.parse(@end_time)).iso8601) if !@end_time.blank?
+    query = query.where('events.start_time >= ?', parsed_date_time(@start_time)) if !@start_time.blank?
+    query = query.where('events.end_time <= ?', parsed_date_time(@end_time)) if !@end_time.blank?
     @results = query.paginate(page: @page)
     @total_entries = @results.total_entries
   end
@@ -34,5 +37,11 @@ class EventSearchService
         venue: event.venue.name
       }}
     }.to_json
+  end
+
+  private
+
+  def parsed_date_time(date_time)
+    DateTime.parse(date_time).iso8601
   end
 end
