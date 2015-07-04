3b427a3bf9f580900d9983374b11c5a5dfb4e261
Jamie Barton <jamie@codethisway.com>
2015-07-04 12:14:49 +0100

    JSON formatted response for /api/venues

--- app/controllers/api/venues_controller.rb
+++ app/controllers/api/venues_controller.rb
@@ -1,5 +1,5 @@
 class API::VenuesController < ApplicationController
   def index
-    render json: Venue.scoped.paginate(page: params[:page])
+    render json: Venue.paginate(page: params[:page]).map { |v| {id: v.id, name: v.name} }
   end
 end
